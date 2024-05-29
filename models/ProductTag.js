c43abe8a44eb2b849853386700ef49509507a991
Clarissa Mobley <clarissamobley@me.com>
2024-05-29 12:06:02 -0400

    added columns in product tag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,23 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal:true,
+      }
+    }
   },
   {
     sequelize,
