d77ade9d3011fe12656fda061320b533ac4ef25e
Clarissa Mobley <clarissamobley@me.com>
2024-05-29 17:24:14 -0400

    Added put to route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,72 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: "No product found witht that id!" });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const catData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catId = req.params.id;
+    const catData = await Category.findByPk(catId);
+
+    if (catData) {
+      await catData.update({
+        category_name: req.body.category_name
+      });
+      res.status(200).json(catData);
+    } else {
+      res.status(404).json({ message: "Category ID not found."});
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
